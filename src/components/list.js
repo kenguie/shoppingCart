ca27f2c77cc85eff20faa0a82db11bfdd73efce2
Ken Guie <nyhunter77@yahoo.com>
2018-09-15 23:42:47 -0400

    adding some logic

--- src/components/list.js
+++ src/components/list.js
@@ -1,38 +1,76 @@
 import React, { Component } from 'React';
 
 class List extends Component {
-	render() {
-		let itemList = [
-			{key:1, name:"apples", amt:3},
-			{key:2, name:"pears", amt:4},
-			{key:3, name:"pecans", amt:1},
-		];
-		itemList.push({key:4, name:"bananas", amt:2});
-		console.log(itemList);
+	constructor(props) {
+		super(props);
+		this.formSubmit = this.formSubmit.bind(this);
+		this.handleClick = this.handleClick.bind(this);
+		this.handleDelete = this.handleDelete.bind(this);
+		this.state = {
+			itemList: [],
+			strike: 'notDone',
+		}
+	}
 
-		itemList.map((item) => {
-			console.log(item);
-			return item;
+	formSubmit(e) {
+		const details = [];
+		e.target.childNodes.forEach(function(el) {
+			if (el.tagName === 'INPUT')
+				details[el.name] = el.value
+			el.value = null
 		})
+		const newItems = this.state.itemList.slice()
+		newItems.push(details)
+		this.setState({
+			itemList: newItems
+		})
+		e.preventDefault()
+	}
+
+	handleClick(e) {
+		if (this.state.strike === 'notDone'){
+			this.setState({
+				strike: 'done'
+			});
+		} else {
+			this.setState({
+				strike: 'notDone'
+			});
+		}
+	}
+
+	handleDelete(index) {
+		console.log(index);
+		const newItems = this.state.itemList.splice(index, 1);
+    this.setState({
+			itemList: newItems
+		});
+	}
+
+	render() {
+		let itemList = [];
+		console.log(this.state.itemList);
 
 		return(
 			<div id="ToDo">
 				<ul>
 					{
-						itemList.map(item => 
-							<li key={item.key}>
+						this.state.itemList.map((item, index) => 
+							<li key={index+1} className={this.state.strike}>
 								<span className='item'>{item.name.toUpperCase()}</span>
 								<span className='amount'>{item.amt}</span>
-								<button className='done'>Done</button>
-								<button className='delete'>Delete</button>
+								<button className='doneButton' onClick={this.handleClick}>Done</button>
+								<button className='deleteButton' onClick={this.handleDelete.bind(this, index+1)}>Delete</button>
 							</li>
 						)
 					}
 				</ul>
 
-				<h2>Enter something you need to purchase</h2>
-				<h2>Enter the amount you need</h2>
-				<button>Submit</button>
+				<form onSubmit={this.formSubmit}>
+					<input type="text" name='name' placeholder='Enter something you need to purchase' />
+					<input type="text" name='amt' placeholder='Enter the amount you need' />
+					<button type='submit' onSubmit={this.handleSubmit}> Submit </button>
+				</form>
 			</div>
 		)
 	}
